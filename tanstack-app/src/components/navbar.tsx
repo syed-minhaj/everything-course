3fe37331fbd2f4990f988c82f8bf0fe8ad271954
syed-minhaj <minhajsulmuneeb@gmail.com>
2026-01-27 12:27:35 +0500

    feat: update padding in navbar

--- tanstack-app/src/components/navbar.tsx
+++ tanstack-app/src/components/navbar.tsx
@@ -21,7 +21,7 @@ function ModeToggle() {
 export default function Navbar() {
 
     return (
-        <div className='flex flex-row h-12 shrink-0 px-8 items-center gap-4 bg-bg1 border border-b-black/50 '>
+        <div className='flex flex-row h-12 shrink-0 px-6 md:px-8 items-center gap-4 bg-bg1 border border-b-black/50 '>
             <Link to="/app" className='text-2xl  font-irish-grover'>
                 Everything Course
             </Link>
