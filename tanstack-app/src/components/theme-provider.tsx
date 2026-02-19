d7d565a6524478ce3bd6764fb6d90866d36e7d4a
syed-minhaj <minhajsulmuneeb@gmail.com>
2026-02-20 02:52:01 +0500

    perf: improving change theme speed

--- tanstack-app/src/components/theme-provider.tsx
+++ tanstack-app/src/components/theme-provider.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "@tanstack/react-router";
-import { createContext, type PropsWithChildren, use } from "react";
+import { createContext, type PropsWithChildren, use, useState } from "react";
 import { setThemeServerFn, type T as Theme } from "@/lib/theme";
 
 type ThemeContextVal = { theme: Theme; setTheme: (val: Theme) => void };
@@ -7,10 +7,12 @@ type Props = PropsWithChildren<{ theme: Theme }>;
 
 const ThemeContext = createContext<ThemeContextVal | null>(null);
 
-export function ThemeProvider({ children, theme }: Props) {
+export function ThemeProvider({ children, theme:Itheme }: Props) {
   const router = useRouter();
+  const [theme, setThemeS] = useState(Itheme);
 
   function setTheme(val: Theme) {
+    setThemeS(val)
     setThemeServerFn({ data: val }).then(() => router.invalidate());
   }
 
